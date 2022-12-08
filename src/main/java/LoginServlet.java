384089074a93e61375f725c998ca131507727296
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-08 16:52:54 +0530

    Refactor: User Valid Pattern By Using Regex

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @WebServlet(urlPatterns = {"/LoginServlet"})
@@ -12,17 +15,33 @@ public class LoginServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         response.setContentType("text/html:charset=UTF-8");
-        try(PrintWriter out=response.getWriter()) {
-            String uname=request.getParameter("username");
-            String password=request.getParameter("password");
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        //get servlet config init params
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        //Check Name Using Regex
+        String nameRegex = "[A-Z]{1}[a-z]{2,}";
+        Pattern npattern = Pattern.compile(nameRegex);
+        Matcher fmatcher = npattern.matcher(user);
 
-            if(uname.equals("Navin") && password.equals("123456")){
-                out.println(("Welcome  "+uname));
+        if(fmatcher.matches() {
+            if (userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either user name or password is wrong.</font>");
+                rd.include(request, response);
             }
-            else {
-                out.println(("Correct User Name and Password"));
-            }
+        }else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either name or password format is wrong.</font>");
+            rd.include(request, response);
         }
+
     }
 
     @Override
